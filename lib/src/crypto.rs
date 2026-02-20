749798aa92f0c46f3ca6fd3e3a9c5bb62805f24a
Didi Prasetyo <didiprasetyo360@gmail.com>
2026-02-20 19:30:43 +0700

    implement miner

--- lib/src/crypto.rs
+++ lib/src/crypto.rs
@@ -7,6 +7,61 @@ use ecdsa::{
 };
 use k256::Secp256k1;
 use serde::{Serialize, Deserialize};
+use spki::EncodePublicKey;
+use std::io::{
+    Error as IoError, ErrorKind as IoErrorKind, Read,
+    Result as IoResult, Write,
+};
+use crate::util::Saveable;
+
+impl Saveable for PrivateKey {
+    fn load<I: Read>(reader: I) -> IoResult<Self> {
+        ciborium::de::from_reader(reader).map_err(|_| {
+            IoError::new(
+                IoErrorKind::InvalidData,
+                "Failed to deserialize PrivateKey",
+            )
+        })
+    }
+
+    fn save<O: Write>(&self, writer: O) -> IoResult<()> {
+        ciborium::ser::into_writer(self, writer).map_err(|_| {
+            IoError::new(
+                IoErrorKind::InvalidData,
+                "Failed to serialize PrivateKey",
+            )
+        })?;
+        Ok(())
+    }
+}
+
+impl Saveable for PublicKey {
+    fn load<I: Read>(mut reader: I) -> IoResult<Self> {
+        let mut buf = String::new();
+        reader.read_to_string(&mut buf)?;
+        let public_key = buf.parse().map_err(|_| {
+            IoError::new(
+                IoErrorKind::InvalidData,
+                "Failed to parse PublicKey"
+            )
+        })?;
+        Ok(PublicKey(public_key))
+    }
+
+    fn save<O: Write>(&self, mut writer: O) -> IoResult<()> {
+        let s = self
+            .0
+            .to_public_key_pem(Default::default())
+            .map_err(|_| {
+                IoError::new(
+                    IoErrorKind::InvalidData,
+                    "Failed to serialize PublicKey",
+                )
+            })?;
+        writer.write_all(s.as_bytes())?;
+        Ok(())
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Signature(ECDSASignature<Secp256k1>);
--- lib/src/network.rs
+++ lib/src/network.rs
@@ -2,6 +2,9 @@ use serde::{Deserialize, Serialize};
 use crate::crypto::PublicKey;
 use crate::types::{Block, Transaction, TransactionOutput};
 use std::io::{Error as IoError, Read, Write};
+use tokio::io::{
+    AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt,
+};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub enum Message {
@@ -9,7 +12,7 @@ pub enum Message {
     UTXOs(Vec<(TransactionOutput, bool)>),
     SubmitTransaction(Transaction),
     NewTransaction(Transaction),
-    FetchTemplate(Transaction),
+    FetchTemplate(PublicKey),
     Template(Block),
     ValidateTemplate(Block),
     TemplateValidity(bool),
@@ -20,4 +23,65 @@ pub enum Message {
     Difference(i32),
     FetchBlock(usize),
     NewBlock(Block),
+}
+
+impl Message {
+    pub fn encode(
+        &self
+    ) -> Result<Vec<u8>, ciborium::ser::Error<IoError>>
+    {
+        let mut bytes = Vec::new();
+        ciborium::into_writer(self, &mut bytes)?;
+        Ok(bytes)
+    }
+
+    pub fn decode(
+        data: &[u8],
+    ) -> Result<Self, ciborium::de::Error<IoError>> {
+        ciborium::from_reader(data)
+    }
+
+    pub fn send(
+        &self,
+        stream: &mut impl Write,
+    ) -> Result<(), ciborium::ser::Error<IoError>> {
+        let bytes = self.encode()?;
+        let len = bytes.len() as u64;
+        stream.write_all(&len.to_be_bytes())?;
+        stream.write_all(&bytes)?;
+        Ok(())
+    }
+
+    pub fn receive(
+        stream: &mut impl Read,
+    ) -> Result<Self, ciborium::de::Error<IoError>> {
+        let mut len_bytes = [0u8; 8];
+        stream.read_exact(&mut len_bytes)?;
+        let len = u64::from_be_bytes(len_bytes) as usize;
+        let mut data = vec![0u8; len];
+        stream.read_exact(&mut data)?;
+        Self::decode(&data)
+    }
+
+    pub async fn send_async(
+        &self,
+        stream: &mut (impl AsyncWrite + Unpin),
+    ) -> Result<(), ciborium::ser::Error<IoError>> {
+        let bytes = self.encode()?;
+        let len = bytes.len() as u64;
+        stream.write_all(&len.to_be_bytes()).await?;
+        stream.write_all(&bytes).await?;
+        Ok(())
+    }
+
+    pub async fn receive_async(
+        stream: &mut (impl AsyncRead + Unpin),
+    ) -> Result<Self, ciborium::de::Error<IoError>> {
+        let mut len_bytes = [0u8; 8];
+        stream.read_exact(&mut len_bytes).await?;
+        let len = u64::from_be_bytes(len_bytes) as usize;
+        let mut data = vec![0u8; len];
+        stream.read_exact(&mut data).await?;
+        Self::decode(&data)
+    }
 }
