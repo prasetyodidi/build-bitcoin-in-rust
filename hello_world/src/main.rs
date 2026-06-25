e5c72414c84edfc7f17b9cff24fc0186258c28f0
Didi Prasetyo <didiprasetyo360@gmail.com>
2026-06-25 16:39:32 +0700

    lifetimes of owned vs unowned values

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -128,7 +128,160 @@ fn main() {
     mut_ref.push_str(", the cryptocurrency");
     // ↑ use the mutable borrow
 
+    let my_ref_hello = my_ref();
+    println!("My reference: {}", my_ref_hello);
+
+    // create string slice from other string slice without copying data by re-borrowing
+    let my_str = "Hello, world!";
+    let hell = &my_str[0..4];
+    println!("{} {}", my_str, hell);
+
+    // this does not compile because string literals are always immutable
+    // let mut_str: &mut str = "Hello, world!";
+
+    let my_string = String::from("Hello, world!");
+    // both work
+    my_fun1(&my_string);
+    my_fun2(&my_string);
+    // however this wouldn't work
+    // my_fun1("Hello!"); <- type mismatch, expected `&String`, got `&str`
+
+    let owned_string = String::from("I'm not static, but i'm owned");
+    print_static(owned_string);
+
+    let text = String::from("Hello, world!");
+    let holder = Holder { reference: &text };
+    println!("{}", holder.reference);
+
+    let x = String::from("Hello, world!");
+    {
+        let y = x;
+        println!("y: {}", y);
+    }
+    // y goes out of scope here, meaning it is dropped, and
+    // deallocated
+    // println!("x: {}", x); Error: x was moved
+    let a = 5; // i32 implements Copy, and so it can be copied
+    {
+        let b = a; // a is copied to b
+        println!("b: {}", b);
+    }
+    println!("a: {}", a); // a is still valid
+
+    let outer;
+    {
+        let owned = String::from("Hello, world!");
+        let reference = &owned;
+        outer = reference; // Error: `owned` does not live long enough
+        println!("{}", reference);
+    } // `owned` is dropped here
+    // println!("{}", outer); // Would not compile
+    let mut ref1 = &5;
+    {
+        let x = 10;
+        ref1 = &x; // ref1 now points to x
+        println!("{}", ref1);
+    } // x is dropped here, ref1 is now dangling
+    // println!("{}", ref1); // Would not compile
+
+    let hello = Hello;
+    (&hello).say_hi();
+    (&&hello).say_hi();
+    hello.say_hi();
+
+    let mut broken = MyStruct {
+        remainder: Some("Hello")
+    };
+    for _ in 0..5 {
+        println!("{:?}", broken.pop_first_char_as_string());
+    }
+
 }
+
+// in the business, we call this foreshadowing ;-)
+struct MyStruct<'a> {
+    remainder: Option<&'a str>,
+}
+// ↑ here, we have created a struct which holds
+// a reference to an Option of a string slice.
+//
+// string slices without the 'static lifetime are views
+// into strings, and cannot outlive them, hence the
+// lifetime parameter
+// impl blocks are used to create methods for types,
+// we need to respect the generic lifetime parameter
+impl<'a> MyStruct<'a> {
+    // this will keep returning the first character
+    fn pop_first_char_as_string(&mut self) -> Option<&str> {
+        // surprise! remainder here gets copied,
+        // so we are not modifying which pointer is
+        // stored in self, but only a copy on the stack
+        let remainder = &mut self.remainder?;
+        let c = &remainder[0..1];
+        if remainder.len() != 1 {
+            *remainder = &remainder[1..];
+            Some(c)
+        } else {
+            self.remainder.take()
+        }
+    }
+}
+
+trait SayHi {
+    fn say_hi(self);
+}
+
+struct Hello;
+
+impl SayHi for Hello {
+    fn say_hi(self) {
+        println!("This hi! will cost me my life - I'm owned value");
+    }
+}
+
+impl SayHi for &Hello {
+    fn say_hi(self) {
+        println!("Hi, I am a reference to Hello!");
+    }
+}
+
+impl SayHi for &&Hello {
+    fn say_hi(self) {
+        println!("Hi, I am a double reference to Hello!");
+    }
+}
+
+struct Holder<'a> {
+    reference: &'a str,
+}
+
+fn print_static<T: 'static + std::fmt::Debug>(value: T) {
+    println!("Value: {:?}", value);
+}
+
+// don't
+fn my_fun1(_input: &String) {}
+// do
+fn my_fun2(_input: &str) {}
+
+fn my_ref() -> &'static str {
+    "Hello, world!"
+}
+
+/*
+fn my_ref1() -> &str {
+    let my_string = String::from("Hello, world!");
+    &my_string
+    // ↑ doesn't compile, return value references temporary value
+}
+ */
+
+/*
+fn my_ref2(input: &String) -> &str {
+    &input
+    // ↑ compiles, since input is known to live longer than the span of this function
+}
+*/
 
 fn takes_reference(my_ref: &String) {
     // <- reference is moved into this function
